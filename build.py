d0072cf161a78fe9ea3c94c933b5266dc678eeb8
Ryan Freckleton <ryan.freckleton@gmail.com>
2014-06-24 16:37:35 -0600

    Broken into individual functions.

--- build.py
+++ build.py
@@ -3,12 +3,24 @@ import glob
 import markdown
 
 def main():
-    paths = glob.glob('*.markdown')
-    for path in paths:
-        content = open(path).read()
-        compiled = markdown.markdown(content)
-        new_path = path.replace('.markdown', '.html')
-        open(new_path, 'w').write(compiled)
+    for path in paths():
+        write_compiled(path)
+
+def paths():
+    return glob.glob('*.markdown')
+
+def read(path):
+    return open(path).read()
+
+def compile(path):
+    return markdown.markdown(read(path))
+
+def new_path(path):
+    return path.replace('.markdown', '.html')
+
+def write_compiled(path):
+    open(new_path(path), 'w').write(compile(path))
+
 
 if __name__ == '__main__':
     main()
